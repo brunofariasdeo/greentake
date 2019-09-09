7f1cae8abf862b37101803351ee7a17e4fbc26b9
Bruno <jbfo@cin.ufpe.br>
2019-09-09 13:11:09 -0300

    Update

--- js/Exercício/e01.js
+++
@@ -1,3 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel} km/h.`)
-console.log('Dirija sempre usando cinto de segurança.')
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//NavBar
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -19,7 +21,10 @@ const navSlide = () => {
 
 function myMap() {
     var mapCenter = {lat: -8.063128, lng: -34.871162};
-    
+    var firstPoint = {lat: -8.063646, lng: -34.871743};
+    var secondPoint = {lat: -8.063930, lng: -34.871686};
+    var thirdPoint = {lat: -8.063273, lng: -34.871766};
+
     var mapProperties= { //defines the properties for the map.
       center:new google.maps.LatLng(mapCenter), //specifies where to center the map (using latitude and longitude coordinates).
       zoom:15 //specifies the zoom level for the map (try to experiment with the zoom level).
@@ -28,34 +33,36 @@ function myMap() {
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProperties);
     
     var sympleBinMarker = new google.maps.Marker({
-        position: mapCenter, 
+        position: firstPoint, 
         map: map,
         icon: 'images/symple-bin.png'
     });
 
     var recycleBinMarker = new google.maps.Marker({
-        position: mapCenter, 
+        position: secondPoint, 
         map: map,
         icon: 'images/recycle-bin.png'
     });
 
     var allRecycleBinMarker = new google.maps.Marker({
-        position: mapCenter, 
+        position: thirdPoint, 
         map: map,
         icon: 'images/all-recycle-bin.png'
     });
     
+    var directionsDisplay = new google.maps.direc
+    var directionsService = new google.maps.directionsService();
 }
 
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+});
 
-// $('.carousel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-// });
-
-
+// initMap();
 navSlide();
 myMap();
